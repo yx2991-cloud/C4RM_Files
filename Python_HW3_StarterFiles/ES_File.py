5a331a2c012d420e486e62c377f4da8ec194aeff
yx2991-cloud <yx2991@columbia.edu>
2026-03-12 13:27:40 -0400

    Update ES_File.py

--- Python_HW3_StarterFiles/ES_File.py
+++ Python_HW3_StarterFiles/ES_File.py
@@ -1,15 +1,13 @@
-
 import numpy as np
 
-def ES(losses, confidence=.95, VaR=None):
-    """
-    Calculate the Expected Shortfall (ES) of losses.
-    
-    :param losses: array of positively stated loss values
-    :param alpha: risk level (e.g., 0.99 for 99%)
-    :param VaR: dollar value or percentage specifying the VaR threshold
-    :return: Expected Shortfall as the average of losses exceeding VaR
-    """
+def ES(losses, confidence=None, VaR=None, use_PnL=False):
 
-    es_value = 90
+        VaR = np.percentile(losses, 100 * confidence)
+    
+    losses_exceeding_var = losses[losses > VaR]
+    
+    if len(losses_exceeding_var) == 0:
+        return 0.0
+    
+    es_value = np.mean(losses_exceeding_var)
     return es_value
